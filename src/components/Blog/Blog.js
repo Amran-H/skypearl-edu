7f652874caf83e519ee8aecd7e26167a8a486e64
Amran Hossain <amran.h.akash@gmail.com>
2022-10-12 20:04:10 +0600

    chart added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import Header from '../Header/Header';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Blog = () => {
     return (
-        <div>
+        <div className='container mt-5'>
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header ><h2 className='text-danger'>What is purpose of react-router?</h2></Accordion.Header>
+                    <Accordion.Body>
+                        <h5>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</h5>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header><h2 className='text-danger'>How does context-API works?</h2></Accordion.Header>
+                    <Accordion.Body>
+                        <h5> The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</h5>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header><h2 className='text-danger'>What is useref hook in react?</h2></Accordion.Header>
+                    <Accordion.Body>
+                        <h5>  The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</h5>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
 
-            Blog
         </div>
     );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import Header from '../Header/Header';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 const Statistics = () => {
+    const data = [
+        {
+            name: "React",
+            total: 8,
+        },
+        {
+            name: "JavaScript",
+            total: 9,
+        },
+        {
+            name: "CSS",
+            total: 8,
+        },
+        {
+            name: "Git",
+            total: 11,
+        },
+    ];
     return (
-        <div>
+        <div className=''>
 
-            stata
+            <LineChart
+                width={380}
+                height={300}
+                data={data}
+            >
+                <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip></Tooltip>
+            </LineChart>
+
         </div>
     );
 };
